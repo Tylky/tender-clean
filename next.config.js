e72c54a6f8cdec53539396f4569bc205573ebaaa
Tylky <tylkytechnologies@gmail.com>
2025-09-16 16:47:52 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,11 @@
 const nextConfig = {}
 
 export default nextConfig
+const res = await fetch(url, {
+  headers: {
+    Authorization: auth,
+    Accept: "application/xml",
+  },
+  cache: "no-store",
+  redirect: "follow", // 🚀 heel belangrijk!
+});
